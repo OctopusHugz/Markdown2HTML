61b8d44756afbdf1074da6c63bb80bcabc3f6d32
OctopusHugz <colsonscott53@gmail.com>
2020-12-23 18:31:10 +0000

    Moved \n to f.write from appending to end of element string

--- markdown2html.py
+++ markdown2html.py
@@ -12,19 +12,18 @@ if __name__ == "__main__":
         exit(1)
     with open(argv[1]) as f:
         file_content = f.readlines()
-    index = 0
     line_array = []
     for line in file_content:
         header_level = line.count("#")
         header_tag = "<h" + str(header_level) + ">"
         closing_header_tag = "</h" + str(header_level) + ">"
         line_content = line.split()
-        line_content[index] = header_tag
         element_text = line_content[1:]
         element_text_string = " ".join(element_text)
         full_element_string = header_tag + element_text_string + \
-            closing_header_tag + "\n"
+            closing_header_tag
         line_array.append(full_element_string)
     with open(argv[2], "w+") as f:
         for line in line_array:
             f.write(line)
+            f.write("\n")
