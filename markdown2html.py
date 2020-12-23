592547a26397a63da6090c0f51fa5422b1b75393
OctopusHugz <colsonscott53@gmail.com>
2020-12-23 17:36:13 +0000

    Added markdown2html.py for 0

---
+++ markdown2html.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv, exit, stderr
+from os import path
+
+if len(argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=stderr)
+    exit(1)
+
+if not (path.exists(argv[1])):
+    print("Missing {}".format(argv[1]), file=stderr)
+    exit(1)
