bc70e3094ae9e1406aab101766025ecc9dfc2067
OctopusHugz <colsonscott53@gmail.com>
2020-12-23 17:37:21 +0000

    Added __main__ guard

--- markdown2html.py
+++ markdown2html.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """This module parses Markdown and converts it to HTML"""
-from sys import argv, exit, stderr
-from os import path
+if __name__ == "__main__":
+    from sys import argv, exit, stderr
+    from os import path
 
-if len(argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=stderr)
-    exit(1)
+    if len(argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
+        exit(1)
 
-if not (path.exists(argv[1])):
-    print("Missing {}".format(argv[1]), file=stderr)
-    exit(1)
+    if not (path.exists(argv[1])):
+        print("Missing {}".format(argv[1]), file=stderr)
+        exit(1)
