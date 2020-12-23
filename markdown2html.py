cb9c95b939b380c3391f90911b04098855a52781
OctopusHugz <colsonscott53@gmail.com>
2020-12-23 18:19:41 +0000

    Updated markdown2html.py for 1

--- markdown2html.py
+++ markdown2html.py
@@ -7,7 +7,23 @@ if __name__ == "__main__":
     if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
-
     if not (path.exists(argv[1])):
         print("Missing {}".format(argv[1]), file=stderr)
         exit(1)
+    with open(argv[1]) as f:
+        file_content = f.readlines()
+    index = 0
+    line_array = []
+    for line in file_content:
+        header_level = line.count("#")
+        header_tag = "<h" + str(header_level) + ">"
+        closing_header_tag = "</h" + str(header_level) + ">"
+        line_content = line.split()
+        line_content[index] = header_tag
+        element_text = line_content[1:]
+        element_text_string = " ".join(element_text)
+        full_element_string = header_tag + element_text_string + closing_header_tag + "\n"
+        line_array.append(full_element_string)
+    with open(argv[2], "w+") as f:
+        for line in line_array:
+            f.write(line)
